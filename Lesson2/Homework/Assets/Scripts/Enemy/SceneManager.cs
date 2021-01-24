08516ae8d38a7abb9f609f84b30b0586306c2bf4
Unity <unity@scenics.app>
2021-01-24 21:14:00 +0300

    Added scene manager

---
+++ Lesson2/Homework/Assets/Scripts/Enemy/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager Instance;
+    
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Restart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene( UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex ) ;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+}
---
+++ Lesson2/Homework/Assets/Scripts/Player/DeathController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var sceneManager = SceneManager.Instance;
+            if (sceneManager != null)
+            {
+                sceneManager.Restart();
+            }
+        }
+    }
+}
