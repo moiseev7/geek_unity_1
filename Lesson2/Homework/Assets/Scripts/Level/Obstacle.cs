84e96fb5d0c9e5c480815faedbf60963a0b2f583
Unity <unity@scenics.app>
2021-01-24 20:31:16 +0300

    Added obstacles

---
+++ Lesson2/Homework/Assets/Scripts/Level/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+
+    [SerializeField] private float _period = 5;
+    private float _lastTime = 0;
+
+    private void Update()
+    {
+        if(_target==null)
+            return;
+        
+        
+        if (Time.time - _lastTime > _period)
+        {
+            _lastTime = Time.time;
+            _target.SetActive(!_target.activeSelf);
+        }
+    }
+}
