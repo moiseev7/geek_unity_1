9053b187c2552a5dc79e8ba605f353c47ea02665
Unity <unity@scenics.app>
2021-01-24 20:08:30 +0300

    Finished player controller

---
+++ Lesson2/Homework/Assets/Scripts/Player/HorizontalMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _rootTransform;
+
+    [SerializeField] private float _speed = 1;
+    
+
+    private Vector3 _movement;
+
+    private void Update()
+    {
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.z = Input.GetAxis("Vertical");
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        if(_rootTransform==null)
+            return;
+        
+        //Если вектор движения близок к нулю, то не двигаемся
+        if(_movement.magnitude<0.01f)
+            return;
+
+        //Поворачиваем объект по направлению движения
+        _rootTransform.forward = _movement.normalized;
+
+        var translate = Vector3.forward * _speed * Time.deltaTime;
+        
+        _rootTransform.Translate(translate);
+        
+    }
+
+}
