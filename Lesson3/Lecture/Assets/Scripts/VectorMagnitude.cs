4ae597f23f91608d9d70052fc82e950036487b3a
Unity <unity@scenics.app>
2021-01-25 09:23:04 +0300

    Added first scene for vector magnitude

---
+++ Lesson3/Lecture/Assets/Scripts/VectorMagnitude.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMagnitude : MonoBehaviour
+{
+    [SerializeField] private Vector2 _vector2D;
+    [SerializeField] private Vector3 _vector3D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Vector2D: {_vector2D.magnitude}");   
+        Debug.Log($"Vector3D: {_vector3D.magnitude}");   
+    }
+}
