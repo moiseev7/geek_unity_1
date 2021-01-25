329e138e74ab8ab7ad4675f7685bd5b597f877ab
Unity <unity@scenics.app>
2021-01-25 09:55:31 +0300

    Finished dot product

---
+++ Lesson3/Lecture/Assets/Scripts/VectorDotAndCrossDemo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class VectorDotAndCrossDemo : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _p2;
+
+    private void OnDrawGizmos()
+    {
+        if(_p1==null || _p2==null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(Vector3.zero, _p1.position);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(Vector3.zero, _p2.position);
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(Vector3.zero, Vector3.Cross(_p1.position,_p2.position));
+        
+        Gizmos.color = Color.white;
+        
+        Gizmos.DrawLine(Vector3.zero, Vector3.right * Vector3.Dot(_p1.position,_p2.position));
+    }
+}
