1f172ab7db82d9141487dc47d8389fae437926e3
Unity <unity@scenics.app>
2021-01-25 09:46:36 +0300

    Finished scenes 1-3

--- Lesson3/Lecture/Assets/Scripts/VectorNormalize.cs
+++ Lesson3/Lecture/Assets/Scripts/VectorNormalize.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class VectorNormalize : MonoBehaviour
 {
+    [SerializeField] private Vector3 _p1;
+    [SerializeField] private Vector3 _p2;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        var direction = (_p2-_p1).normalized;
+        Debug.Log($"Normalize: {direction}");
+        Debug.Log($"Magnitude: {direction.magnitude}");
         
     }
 }
---
+++ Lesson3/Lecture/Assets/Scripts/VectorNormalizeDemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class VectorNormalizeDemo : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _p2;
+
+    private void OnDrawGizmos()
+    {
+        if(_p1==null || _p2==null)
+            return;
+        
+        Gizmos.color = Color.white;
+        
+        Gizmos.DrawLine(_p1.position,_p2.position);
+        
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawLine(Vector3.zero, (_p2.position - _p1.position).normalized);
+    }
+}
