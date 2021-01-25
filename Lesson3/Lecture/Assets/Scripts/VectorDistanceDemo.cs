a3d0b8c5647da28134d256aec320ca51cdfe4adf
Unity <unity@scenics.app>
2021-01-25 09:41:47 +0300

    Scenes 1-3 are in progress

---
+++ Lesson3/Lecture/Assets/Scripts/VectorDistanceDemo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class VectorDistanceDemo : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _p2;
+
+    private void OnDrawGizmos()
+    {
+        if(_p1==null || _p2==null)
+            return;
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawLine(Vector3.zero, Vector3.right * (_p1.position-_p2.position).magnitude);
+        
+        Gizmos.color = Color.blue;
+        
+        Gizmos.DrawLine(Vector3.up, Vector3.right * Vector3.Distance(_p1.position,_p2.position) + Vector3.up);
+        
+        Gizmos.color = Color.green;
+        
+        Gizmos.DrawLine(Vector3.up*2, Vector3.right * (_p1.position-_p2.position).sqrMagnitude + Vector3.up*2);
+        
+        Gizmos.color = Color.white;
+        
+        Gizmos.DrawLine(_p1.position,_p2.position);
+    }
+}
---
+++ Lesson3/Lecture/Assets/Scripts/VectorMagnitudeDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMagnitudeDemo : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _p2;
+
+    private void OnDrawGizmos()
+    {
+        if(_p1==null || _p2==null)
+            return;
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawLine(Vector3.zero, Vector3.right * (_p1.position-_p2.position).magnitude);
+        
+        Gizmos.color = Color.white;
+        
+        Gizmos.DrawLine(_p1.position,_p2.position);
+    }
+}
