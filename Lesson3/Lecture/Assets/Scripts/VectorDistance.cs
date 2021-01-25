256a9d8c5941eb9a9cffb4c27e2911cb0b73d5b1
Unity <unity@scenics.app>
2021-01-25 09:29:24 +0300

    Added scene 2

---
+++ Lesson3/Lecture/Assets/Scripts/VectorDistance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class VectorDistance : MonoBehaviour
+{
+    [SerializeField] private Vector3 _p1;
+    [SerializeField] private Vector3 _p2;
+
+    private void Start()
+    {
+        Debug.Log($"Distance: {Vector3.Distance(_p1,_p2)}");
+        Debug.Log($"Magnitude: {(_p1-_p2).magnitude}");
+        Debug.Log($"SQRMagnitude: {(_p1-_p2).sqrMagnitude}");
+    }
+}
