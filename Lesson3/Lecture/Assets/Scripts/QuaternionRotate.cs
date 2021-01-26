d0b402a55ed4be06cc7efea851b17a031a5ddb59
Unity <unity@scenics.app>
2021-01-26 18:52:06 +0300

    Updated scene 12

--- Lesson3/Lecture/Assets/Scripts/QuaternionRotate.cs
+++ Lesson3/Lecture/Assets/Scripts/QuaternionRotate.cs
@@ -6,15 +6,17 @@ public class QuaternionRotate : MonoBehaviour
     
     [SerializeField] private Transform _local;
     [SerializeField] private Transform _world;
+    [SerializeField] private Transform _separate;
     
     // Start is called before the first frame update
     void Update()
     {
-        if(_local==null || _world==null)
+        if(_local==null || _world==null || _separate==null)
             return;
         
         _local.Rotate(Vector3.up * Time.deltaTime * _speed);
         _world.Rotate(Vector3.up * Time.deltaTime * _speed,Space.World);
+        _separate.Rotate(Time.deltaTime * _speed,0,0);
     }
 
 }
