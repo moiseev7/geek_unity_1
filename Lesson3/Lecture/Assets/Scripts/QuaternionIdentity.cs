fa7556c0d1acd18003eca4703dfa5fdc6c45b917
Unity <unity@scenics.app>
2021-01-26 18:29:10 +0300

    Added quaternion.identity

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionIdentity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionIdentity : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_p1==null)
+            return;
+        
+        _p1.rotation = Quaternion.identity;
+    }
+
+}
