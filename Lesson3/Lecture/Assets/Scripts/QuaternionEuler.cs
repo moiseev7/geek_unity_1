738bdb0ce5616c6c5bced733c4dc6d080b6285b2
Unity <unity@scenics.app>
2021-01-26 18:37:49 +0300

    Added scene 9

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionEuler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class QuaternionEuler : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_p1==null)
+            return;
+        
+        _p1.rotation = Quaternion.Euler(0,30,0);
+    }
+
+}
