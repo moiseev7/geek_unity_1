0bc4e36553e0ea393958a5f97fff8ff1566c8bf3
Unity <unity@scenics.app>
2021-01-26 18:36:07 +0300

    Added scene 8

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionAngle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class QuaternionAngle : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _p2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_p1==null || _p2==null)
+            return;
+        
+        Debug.Log(Quaternion.Angle(_p1.rotation,_p2.rotation));
+    }
+
+}
