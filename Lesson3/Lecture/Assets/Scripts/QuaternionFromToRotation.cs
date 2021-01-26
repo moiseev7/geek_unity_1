38695200646c299345982dbcfaff4caa6ab7ef71
Unity <unity@scenics.app>
2021-01-26 18:45:42 +0300

    Added scene 11

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionFromToRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class QuaternionFromToRotation : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_p1==null)
+            return;
+        
+        _p1.rotation = Quaternion.FromToRotation(Vector3.up, Vector3.right);
+    }
+
+}
