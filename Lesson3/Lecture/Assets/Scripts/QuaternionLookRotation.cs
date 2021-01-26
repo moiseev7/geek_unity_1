124237fd3ddcd34ee6124a1b92a5b437df3443fe
Unity <unity@scenics.app>
2021-01-26 18:33:03 +0300

    Finished scene 7

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionLookRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class QuaternionLookRotation : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_p1==null || _target==null)
+            return;
+
+        var relativePosition = _target.position - _p1.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePosition);
+        _p1.rotation = rotation;
+    }
+
+}
