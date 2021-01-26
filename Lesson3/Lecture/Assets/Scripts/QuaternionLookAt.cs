ecda34cb485ce19e85aa98dc1553f34c36bab24b
Unity <unity@scenics.app>
2021-01-26 19:06:23 +0300

    Finished scene 15

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionLookAt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class QuaternionLookAt : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _target;
+    
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(_p1==null || _target==null)
+            return;
+        
+        _p1.LookAt(_target);
+
+    }
+
+}
