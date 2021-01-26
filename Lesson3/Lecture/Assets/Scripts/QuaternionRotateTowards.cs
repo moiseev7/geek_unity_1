1659067cd1090d5b93af1e01a8af1a0e4139860d
Unity <unity@scenics.app>
2021-01-26 19:01:39 +0300

    Added scene 13

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionRotateTowards.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class QuaternionRotateTowards : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _target;
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(_p1==null || _target==null)
+            return;
+            
+        
+        Vector3 targetDir = _target.position - _p1.position;
+
+        Vector3 newDir = Vector3.RotateTowards(_p1.forward, targetDir, _speed * Time.deltaTime, 1);
+        Debug.DrawRay(_p1.position, newDir, Color.red);
+
+        _p1.rotation = Quaternion.LookRotation(newDir);
+
+    }
+
+}
