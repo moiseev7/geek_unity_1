ed914ae46b08473eae11f433246bb9c0afc9d7fe
Unity <unity@scenics.app>
2021-01-26 18:40:54 +0300

    Added scene 10

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionSlerp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class QuaternionSlerp : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _from;
+    [SerializeField] private Transform _to;
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(_p1==null || _from ==null || _to==null)
+            return;
+        
+        _p1.rotation = Quaternion.Slerp(_from.rotation, _to.rotation,     
+            Time.time);
+
+    }
+
+}
