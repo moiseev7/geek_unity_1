2559d1a63fe0b7a17489c88db6852853dc125a92
Unity <unity@scenics.app>
2021-01-26 19:04:51 +0300

    Added scene 14

---
+++ Lesson3/Lecture/Assets/Scripts/QuaternionRotateAround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class QuaternionRotateAround : MonoBehaviour
+{
+    [SerializeField] private Transform _p1;
+    [SerializeField] private Transform _target;
+    
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(_p1==null || _target==null)
+            return;
+        
+        _p1.RotateAround(_target.position, Vector3.up, 10 * 
+                                                          Time.deltaTime);
+
+    }
+
+}
