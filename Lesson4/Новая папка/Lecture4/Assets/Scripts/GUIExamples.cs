8b4ee0459a0e8a29d453397887d532036f8ac39f
Unity <unity@scenics.app>
2021-01-28 20:20:18 +0300

    Finished scene 5

---
+++ Lesson4/Новая папка/Lecture4/Assets/Scripts/GUIExamples.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIExamples : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Start Invoke"))
+        {
+            //Поиск методоа по имени это дорого
+            Invoke("MyInvokeFunction", 1.0f);
+        }
+        if (GUILayout.Button("Start InvokeRepeating"))
+        {
+            InvokeRepeating("MyInvokeRepeatingFunction", 1.0f, 2.0f);
+        }
+        if (GUILayout.Button("Cancel Invoke"))
+        {
+            CancelInvoke(); // Останавливает все запущенные через Invoke функции. Можно указать конкретную функцию, которую необходимо остановить
+        }
+    }
+    private void MyInvokeFunction()
+    {
+        print("Invoke");
+    }
+    private void MyInvokeRepeatingFunction()
+    {
+        print("RepeatingInvoke "+Time.time);
+    }
+
+}
