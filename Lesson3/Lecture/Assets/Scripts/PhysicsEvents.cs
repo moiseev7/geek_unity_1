83f1f20f21b7ff0611bbf937e9f136bd3c51cbf3
Unity <unity@scenics.app>
2021-01-28 18:12:20 +0300

    finished scene 16

---
+++ Lesson3/Lecture/Assets/Scripts/PhysicsEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhysicsEvents : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{gameObject.name} OnTriggerEnter with {other.name} ");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log($"{gameObject.name} OnTriggerStay with {other.name} ");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"{gameObject.name} OnTriggerExit with {other.name} ");
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log($"{gameObject.name} OnCollisionEnter with {other.collider.name} ");
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        
+        
+        Debug.Log($"{gameObject.name} OnCollisionStay with {other.collider.name} ");
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log($"{gameObject.name} OnCollisionExit with {other.collider.name} ");
+    }
+}
