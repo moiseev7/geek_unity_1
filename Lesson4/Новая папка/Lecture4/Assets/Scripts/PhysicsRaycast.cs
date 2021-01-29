a67b27a8d3f14c402a008200ee74a5938809550e
Unity <unity@scenics.app>
2021-01-29 17:58:40 +0300

    Added scene 7

---
+++ Lesson4/Новая папка/Lecture4/Assets/Scripts/PhysicsRaycast.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicsRaycast : MonoBehaviour
+{
+    [SerializeField] private LayerMask _mask;
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _distance = 10;
+    Color _color = Color.green;
+    
+    public UnityEvent OnFoundPlayer;
+    public UnityEvent OnLostPlayer;
+    public bool _foundPlayer;
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        var startPos = transform.position;
+        var dir = _player.position - startPos;
+
+
+        var rayCast = Physics.Raycast(startPos, dir, out hit, _distance, _mask);
+
+        _color = Color.green;
+        var foundPlayerThisFrame = false;
+        
+        if (rayCast)
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                _color = Color.red;
+                foundPlayerThisFrame = true;
+            }
+        }
+
+        if (foundPlayerThisFrame != _foundPlayer)
+        {
+            UpdateState(foundPlayerThisFrame);
+        }
+        
+        //Debug.DrawRay(_caster.position, dir, _color);
+    }
+
+    private void UpdateState(bool foundPlayerThisFrame)
+    {
+        _foundPlayer = foundPlayerThisFrame;
+        if (_foundPlayer)
+        {
+            OnFoundPlayer.Invoke();
+            Debug.Log("Found player");
+            return;
+        }
+        OnLostPlayer.Invoke();
+        Debug.Log("LostPlayer player");
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _color;
+        
+        Gizmos.DrawLine(transform.position, transform.position + (_player.position - transform.position).normalized * _distance);
+        
+    }
+}
+    
