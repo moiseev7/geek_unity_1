9ecab45123bf64ea971f2bcf9dd880dcc242e39c
Unity <unity@scenics.app>
2021-02-08 19:32:11 +0300

    Finished scenes 7 & 8

---
+++ Lesson7/Lesson7/Assets/Scripts/ToggleTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTest : MonoBehaviour
+{
+    public void SetToggle(bool toggle)
+    {
+        var text = toggle ? "on" : "off";
+        Debug.Log($"Toggle is {text}");
+    }
+}
