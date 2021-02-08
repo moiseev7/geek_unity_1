d26d0ef48875f671de7e99bdb25e9e9fd8384e24
Unity <unity@scenics.app>
2021-02-08 19:18:12 +0300

    Finished scenes 5 & 6

---
+++ Lesson7/Lesson7/Assets/Scripts/ImageSwapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwapper : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _sprites;
+    [SerializeField] private Image _image;
+    private int _index = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _index = ++_index % _sprites.Count;
+
+            _image.sprite = _sprites[_index];
+        }
+    }
+}
---
+++ Lesson7/Lesson7/Assets/Scripts/SurpiseText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurpiseText : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _text.text = Random.Range(0, 10000).ToString();
+        }
+    }
+}
