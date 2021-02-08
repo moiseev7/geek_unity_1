0433e3c0890a3732070bb0ca66c196db580237c7
Unity <unity@scenics.app>
2021-02-08 20:25:27 +0300

    Finished

---
+++ Lesson7/Lesson7/Assets/Scripts/DropdownTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownTest : MonoBehaviour
+{
+    [SerializeField] private Dropdown _dropdown;
+    
+    public void SetDropDown(int index)
+    {
+        
+        Debug.Log($"Drop down value is : {_dropdown.options[index].text}");
+    }
+}
---
+++ Lesson7/Lesson7/Assets/Scripts/InputTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTest : MonoBehaviour
+{
+    [SerializeField] private InputField _inputField;
+    
+    public void OnType(string text)
+    {
+        Debug.Log($"Typing: {text}");
+    }
+    
+    public void OnFinished(string text)
+    {
+        Debug.Log($"Finished: {text}");
+        Debug.Log($"The text was '{_inputField.text}'");
+    }
+}
