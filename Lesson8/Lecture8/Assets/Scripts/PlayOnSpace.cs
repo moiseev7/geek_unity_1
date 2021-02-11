551070adbd1c145b4c4f0b5212ac645f14c94f5a
Unity <unity@scenics.app>
2021-02-11 21:34:41 +0300

    Lecture8 before import

---
+++ Lesson8/Lecture8/Assets/Scripts/PlayOnSpace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnSpace : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _audioSource.Play();
+        }
+    } 
+
+}
